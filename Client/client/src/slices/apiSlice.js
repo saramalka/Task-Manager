07cc1e2439e1606f54a23692da51d3d29503bf8a
saramalka <sari.shlomovitz@gmail.com>
2025-04-29 20:52:51 +0300

    add team slice

--- Client/client/src/slices/apiSlice.js
+++ Client/client/src/slices/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const apiSlice=createApi({
     reducerPath:'api',
-    baseQuery:fetchBaseQuery("http://localhost:2200"),
+    baseQuery:fetchBaseQuery({
+        baseUrl:"http://localhost:2200"
+    }),
     endpoints:()=>({})
 
 })
--- Client/client/src/slices/teamSlice.js
+++ Client/client/src/slices/teamSlice.js
@@ -5,9 +5,55 @@ const teamApiSlice=apiSlice.injectEndpoints({
         getTeams:build.query({
             query:()=>({
                 url:"/api/team"
-            })
-        })
+            }),
+            providesTags:["Teams"]
+        }),
+        getTeamById:build.query({
+            query:(id)=>({
+                url:`/api/team/${id}`
+            }),
+            
+        }),
+        deleteTeam:build.mutation({
+            query:(team)=>({
+                url:`/api/team/${team._id}`,
+                method:'DELETE',
+                body:team
+        }),
+        invalidatesTags:["Teams"]
+        }),
+        deleteTeams: build.mutation({
+            query: (ids) => ({
+              url: `/teams`,
+              method: 'DELETE',
+              body: ids,
+            }),
+            invalidatesTags: ['Teams'],
+          }),
+        editTeam:build.mutation({
+            query:(team)=>({
+                url:`api/team`,
+                method:"PUT",
+                body:team
+            }),
+            invalidatesTags:["Teams"]
+        }),
+        addMemberToTeam: build.mutation({
+          query: ({ teamId, userId }) => ({
+            url: `/api/team/${teamId}/members`,
+            method: "POST",
+            body: { userId },
+          }),
+          invalidatesTags: ["Teams"],
+        }),
+        removeMemberFromTeam: build.mutation({
+          query: ({ teamId, userId }) => ({
+            url: `/api/team/${teamId}/members/${userId}`,
+            method: "DELETE",
+          }),
+          invalidatesTags: ["Teams"],
+        }),  
     })
 })
 
-export const {useGetTeamsQuery}=teamApiSlice
+export const {useGetTeamsQuery,useDeleteTeamMutation,useDeleteTeamsMutation,useEditTeamMutation,useGetTeamByIdQuery,useAddMemberToTeamMutation,useRemoveMemberFromTeamMutation}=teamApiSlice
